5c739c40fc2f77dd8da757feb7e6eb16d0b613e6
sirStephen <itobitoast@gmail.com>
2020-05-02 16:28:35 +0100

    implement delete user functionality

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -4,7 +4,8 @@ const router = express.Router();
 const userController = require('../controllers/user.controller');
 
 router.get('/users', userController.allUsers);
-router.post('/users/login', userController.loginUser);
-router.post('/users/create', userController.createUser);
+router.post('/users', userController.loginUser);
+router.post('/users', userController.createUser);
+router.delete('/users/:id', userController.deleteUser);
 
 module.exports = router;
