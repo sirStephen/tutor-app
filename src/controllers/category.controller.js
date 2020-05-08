d43252764da9c993a4d7d4543131fafdede637e5
sirStephen <itobitoast@gmail.com>
2020-05-08 23:52:55 +0100

    get all categories

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -19,6 +19,20 @@ const createCategory = async (req, res) => {
         .catch(err => console.log(err))
 }
 
+const allCategories = async (req, res) => {
+    Category.find()
+        .then(result => {
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                error: err
+            });
+        });
+}
+
 module.exports = {
-    createCategory
+    createCategory,
+    allCategories
 }
--- src/routes/category.route.js
+++ src/routes/category.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const categoryController = require('../controllers/category.controller');
 
-// router.get('/users', userController.allUsers);
+router.get('/category', userController.allCategory);
 router.post('/category/create', categoryController.createCategory);
 // router.delete('/users/:id', userController.deleteUser);
 
