dac2ac868f933b35f14afe2195da19d09b274111
sirStephen <itobitoast@gmail.com>
2020-05-09 00:02:43 +0100

    fix typo

--- src/routes/category.route.js
+++ src/routes/category.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const categoryController = require('../controllers/category.controller');
 
-router.get('/category', userController.allCategory);
+router.get('/category', categoryController.allCategory);
 router.post('/category/create', categoryController.createCategory);
 // router.delete('/users/:id', userController.deleteUser);
 
