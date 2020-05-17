c61d50658f04680d34272d31ab20c3a63f8837e1
sirStephen <itobitoast@gmail.com>
2020-05-17 09:36:40 +0100

    modify course model

--- src/models/course.model.js
+++ src/models/course.model.js
@@ -4,10 +4,6 @@ const Schema = mongoose.Schema;
 const courseSchema = new Schema(
     {
         course: { type: String, required: true },
-        category: [{ 
-            type: mongoose.Schema.Types.ObjectId, 
-            ref: 'Category' 
-        }]
     }
 );
 
